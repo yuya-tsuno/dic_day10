409055c344027c9e557fbb03ce74b1f98f7f2915
manish kumar <manikuma0405@gmail.com>
2020-01-19 11:39:35 +0900

    アソシエションの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_18_121246) do
+ActiveRecord::Schema.define(version: 2020_01_19_020504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,10 @@ ActiveRecord::Schema.define(version: 2020_01_18_121246) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +31,5 @@ ActiveRecord::Schema.define(version: 2020_01_18_121246) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "blogs", "users"
 end
