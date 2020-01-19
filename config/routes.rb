8a84bcb7a2b87b692429266d0fca475976b418db
manish kumar <manikuma0405@gmail.com>
2020-01-19 10:54:48 +0900

    root toを追加しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-	resources :sessions, only: [:new, :create, :destroy]
-=======
+  resources :sessions, only: [:new, :create, :destroy]
+
   resources :blogs
   resources :users
+root to:"blogs#index"
+#manish ルートを追加しました
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
->>>>>>> 79130a1d70115aa0ba483fd11502eefec197402d
 end
